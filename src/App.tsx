764d4d95bc46ba4c9ed488d6c86e7a4507bac6b9
dmtriyevdenys <dmytriyedenys@gmail.com>
2023-07-26 18:37:12 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import s from "./App.module.scss"
 
 export const App: React.FC = () => { 
 
     return ( 
-        <div>
-            <h1>Перший TS проєкт</h1>
+        <div className={s.container}>
+            <p >Перший TS проєкт</p>
+           <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur numquam soluta ducimus doloribus. Modi molestias deserunt, doloremque obcaecati quae suscipit asperiores incidunt dolorum nulla quod libero tempora quisquam enim!
+           </p>
         </div>
     )
 }
